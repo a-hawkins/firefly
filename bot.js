202843903cb865730e77053c85132f870366a210
KirbySteaks <kirbysteaks@gmail.com>
2019-07-31 19:37:56 -0500

    Added slight organization and message prefix requirement

--- bot.js
+++ bot.js
@@ -3,12 +3,27 @@ const client = new Discord.Client();
 const auth = require('./auth.json');
 const prefix = "f!";
 
+function isToBot(msg) {
+    return (msg.content.startsWith(prefix));
+};
+
+
+function processMsg(msg)
+{
+    if (msg.content.length<3)
+        return;
+    const content = msg.content.substr(2);
+
+    if (content === 'ping') {
+        msg.reply('pong');
+    }
+}
 client.on('ready', () => {
     console.log(`Logged in as ${client.user.tag}!`);
 });
 client.on('message', msg => {
-    if (msg.content === 'ping') {
-      msg.reply('pong');
+    if (isToBot(msg)) {
+      processMsg(msg);
     }
 });
 
