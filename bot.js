307566b034392322cc5c9a56bd361aec7fabf115
KirbySteaks <kirbysteaks@gmail.com>
2019-08-01 01:37:42 -0500

    Added placeholder for help command

--- bot.js
+++ bot.js
@@ -7,17 +7,27 @@ function isToBot(msg) {
     return (msg.content.startsWith(prefix));
 };
 
-
 function processMsg(msg)
 {
     if (msg.content.length<3)
         return;
-    const content = msg.content.substr(2).toLowerCase();
-
-    if (content === 'ping') {
-        msg.reply('pong');
+    const content = msg.content.substr(2).toLowerCase().split(' ');
+    const command = content[0]
+    if (command === 'ping') {
+        msg.channel.send('pong')
+        return('ping');
+    }
+    if (command === 'help')
+    {
+        help(msg);
+        return('help');
     }
 }
+function help(msg)
+{
+    msg.channel.send('Help is on the way!');
+}
+
 client.on('ready', () => {
     console.log(`Logged in as ${client.user.tag}!`);
 });
